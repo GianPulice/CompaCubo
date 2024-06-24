cd5965559ffe8f59d9d89011171ea466dbae9ea6
GianPulice <gpulice.xx@gmail.com>
2024-06-24 12:04:42 -0300

    Revert "level 2 con tilemaps"
    This reverts commit c4fd36542ebff10e47a003b1e6661e3d1c7061dc.

--- Assets/Scipts/NextLevel.cs
+++ Assets/Scipts/NextLevel.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
@@ -15,21 +14,5 @@ public class NextLevel : MonoBehaviour
     void Update()
     {
         
-    }
-    [ContextMenu("cambiar nivel")]
-    public void nextlevel()
-    {
-        int LevelNow = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(LevelNow+1);
-    }
-
-
-    public void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "next")
-        {
-            int LevelNow = SceneManager.GetActiveScene().buildIndex;
-            SceneManager.LoadScene(LevelNow + 1);
-        }
     }
 }
