c4fd36542ebff10e47a003b1e6661e3d1c7061dc
Pirizjrs <fpiriz625@gmail.com>
2024-06-24 11:50:32 -0300

    level 2 con tilemaps

--- Assets/Scipts/NextLevel.cs
+++ Assets/Scipts/NextLevel.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
@@ -14,5 +15,21 @@ public class NextLevel : MonoBehaviour
     void Update()
     {
         
+    }
+    [ContextMenu("cambiar nivel")]
+    public void nextlevel()
+    {
+        int LevelNow = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(LevelNow+1);
+    }
+
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "next")
+        {
+            int LevelNow = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(LevelNow + 1);
+        }
     }
 }
