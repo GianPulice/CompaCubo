a45ed7cb1f6880ebd4d696a6966f60d9a95f400c
Pirizjrs <fpiriz625@gmail.com>
2024-06-25 17:15:20 -0300

    modificacion de scripts

--- Assets/Scipts/Finish.cs
+++ Assets/Scipts/Finish.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
     public void Exit() 
     {
         Application.Quit();
+    }
+    public void Cambio()
+    {
+        SceneManager.LoadScene(4);
     }
 }
--- Assets/Scipts/NextLevel.cs
+++ Assets/Scipts/NextLevel.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class NextLevel : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -14,5 +14,10 @@ public class NextLevel : MonoBehaviour
     void Update()
     {
         
+    }
+    public void CambioNivel()
+    {
+        SceneManager.LoadScene(2);
+
     }
 }
